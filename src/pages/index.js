c8831fe4645ebef20d64af2dc7c0105304da5055
gekal <liu.hongying@hotmail.com>
2024-11-25 21:34:59 +0900

    Refactor project structure for improved organization
    Moved various files and directories under a new src/ directory to streamline the project's structure. Updated all import paths to reflect the new file locations, ensuring consistent and relative paths throughout the codebase. This change improves maintainability and clarity of the code organization.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
-import Layout, {siteTitle} from '../components/layout';
+import Layout, {siteTitle} from '../../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import {getSortedPostsData} from '../lib/posts';
-import Date from '../components/date';
+import Date from '../../components/date';
 import Link from "next/link";
 
 export default function Home({allPostsData}) {
